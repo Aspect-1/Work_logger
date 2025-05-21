3443c01947dd1dee39930228545cef1e8fdc09b1
Rahul <rahulr220534@gmail.com>
2025-05-22 00:39:29 +0530

    Your meaningful commit message describing changes

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
+const path = require('path');
 const userRouter = require('./routes/user.routes');
 const mainRouter = require('./routes/main.routes');
-
 
 const dotenv = require('dotenv');
 dotenv.config();
@@ -9,23 +9,25 @@ const connectToDB = require('./config/db');
 connectToDB();
 const cookieParser = require('cookie-parser');
 
-
-
 const app = express();
 
 app.use(express.static('public'));
 
+// Serve models folder statically for face-api.js
+app.use('/models', express.static(path.join(__dirname, 'public/models')));
 
 app.set('view engine', 'ejs');
 app.use(express.json());
-app.use(cookieParser())
+app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 
 app.use('/user', userRouter);
 app.use('/', mainRouter);
 
-
+app.get('/test-model', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/models/tiny-face-detector/tiny_face_detector_model-weights_manifest.json'));
+});
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
-})
+});
